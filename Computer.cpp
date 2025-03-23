3d84e74f900dfca5275f1ca408927473a656a26b
Zemphis <fardeen.15.l1f3@gmail.com>
2025-03-23 17:52:28 +1030

    clear

--- Computer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Computer.h"
-
-Computer::Computer() {}
-Computer::~Computer() {}
-
-Move* Computer::makeMove() { return new Rock(); }
-
-std::string Computer::getName() const { return "Computer"; }
