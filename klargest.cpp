7bde14e0bc792c720a2111f6cdd4066e6f12e588
Zemphis <fardeen.15.l1f3@gmail.com>
2025-05-25 22:35:42 +0930

    all

--- klargest.cpp
+++ klargest.cpp
@@ -1,20 +1,16 @@
 #include <queue>
 #include <vector>
 
-int kthLargest(const std::vector<int>& nums, int k) {
+int kth_largest(std::vector<int> nums, int k) {
   // Create a min-heap using a priority queue
   std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
 
   // Insert the first k elements into the min-heap
-  for (int i = 0; i < k; ++i) {
-    minHeap.push(nums[i]);
-  }
-
-  // Process the remaining elements
-  for (int i = k; i < nums.size(); ++i) {
-    if (nums[i] > minHeap.top()) {
+  for (int val : nums) {
+    minHeap.push(val);
+    // If the size of the heap exceeds k, remove the smallest element
+    if (minHeap.size() > k) {
       minHeap.pop();
-      minHeap.push(nums[i]);
     }
   }
 
