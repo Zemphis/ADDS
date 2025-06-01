c4438ef30a4fdabb6bbdcaae1830b79a55b93f0b
Zemphis <fardeen.15.l1f3@gmail.com>
2025-06-01 20:39:54 +0930

    all

--- DocumentManager.h
+++ DocumentManager.h
@@ -1,6 +1,7 @@
 #ifndef DOCUMENT_MANAGER_H
 #define DOCUMENT_MANAGER_H
 
+#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
@@ -27,11 +28,28 @@ class DocumentManager {
 
  public:
   DocumentManager() = default;
+
   void addDocument(std::string name, int id, int licenseLimit);
   void addPatron(int patronId);
   int search(std::string name);
   bool borrowDocument(int patronId, int documentId);
   void returnDocument(int patronId, int documentId);
+  void printDocumentState(int documentId) const {
+    auto it = documents.find(documentId);
+    if (it != documents.end()) {
+      const Document& doc = it->second;
+      std::cout << "Document ID: " << doc.id << ", Name: " << doc.name
+                << ", License Limit: " << doc.licenseLimit
+                << ", Current Patrons: ";
+      for (int patron : doc.currentPatrons) {
+        std::cout << patron << " ";
+      }
+      std::cout << std::endl;
+    } else {
+      std::cout << "Document with ID " << documentId << " not found."
+                << std::endl;
+    }
+  }
 };
 
 #endif
